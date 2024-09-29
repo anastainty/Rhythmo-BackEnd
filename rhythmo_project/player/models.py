67aadd286bdf010097cda1cedf25b94509d73bb5
Matsvei Kisin <kisinmatvei@gmail.com>
2024-09-29 19:20:43 +0300

    updated models

--- rhythmo_project/player/models.py
+++ rhythmo_project/player/models.py
@@ -1,8 +1,8 @@
 from django.db import models
 
 class User(models.Model):
-    username = models.CharField(max_length=255, unique=True)
-    email = models.EmailField(unique=True)
+    username = models.CharField(max_length=255, db_index=True)
+    email = models.EmailField(max_length=255, unique=True, db_index=True)
     password_hash = models.TextField()
     avatar_path = models.TextField(blank=True, null=True)
     created_at = models.DateTimeField(auto_now_add=True)
@@ -12,7 +12,7 @@ class User(models.Model):
         return self.username
 
 class Artist(models.Model):
-    name = models.CharField(max_length=255)
+    name = models.CharField(max_length=255, db_index=True)
     bio = models.TextField(blank=True, null=True)
     avatar_path = models.TextField(blank=True, null=True)
     created_at = models.DateTimeField(auto_now_add=True)
@@ -21,17 +21,16 @@ class Artist(models.Model):
     def __str__(self):
         return self.name
 
+
 class Genre(models.Model):
-    name = models.CharField(max_length=255)
+    name = models.CharField(max_length=255, db_index=True)
 
     def __str__(self):
         return self.name
 
 class Album(models.Model):
-    title = models.CharField(max_length=255)
-    artist = models.ForeignKey(Artist, on_delete=models.CASCADE)
+    title = models.CharField(max_length=255, db_index=True)
     release_date = models.DateField(blank=True, null=True)
-    duration = models.TimeField(blank=True, null=True)
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
 
@@ -39,9 +38,9 @@ class Album(models.Model):
         return self.title
 
 class Track(models.Model):
-    title = models.CharField(max_length=255)
-    album = models.ForeignKey(Album, on_delete=models.CASCADE, null=True, blank=True)
-    genre = models.ForeignKey(Genre, on_delete=models.SET_NULL, null=True, blank=True)
+    title = models.CharField(max_length=255, db_index=True)
+    album = models.ForeignKey(Album, on_delete=models.CASCADE, blank=True, null=True)
+    genre = models.ForeignKey(Genre, on_delete=models.SET_NULL, blank=True, null=True)
     duration = models.TimeField(blank=True, null=True)
     file = models.FileField(upload_to='songs/', null=True, blank=True)
     release_date = models.DateField(blank=True, null=True)
@@ -52,7 +51,7 @@ class Track(models.Model):
         return self.title
 
 class Playlist(models.Model):
-    name = models.CharField(max_length=255)
+    name = models.CharField(max_length=255, db_index=True)
     user = models.ForeignKey(User, on_delete=models.CASCADE)
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
@@ -75,3 +74,50 @@ class ListeningHistory(models.Model):
 
     def __str__(self):
         return f'{self.user.username} - {self.track.title}'
+
+class ArtistGenre(models.Model):
+    artist = models.ForeignKey(Artist, on_delete=models.CASCADE)
+    genre = models.ForeignKey(Genre, on_delete=models.CASCADE)
+
+    class Meta:
+        unique_together = ('artist', 'genre')
+
+
+class ArtistTrack(models.Model):
+    artist = models.ForeignKey(Artist, on_delete=models.CASCADE)
+    track = models.ForeignKey(Track, on_delete=models.CASCADE)
+
+    class Meta:
+        unique_together = ('artist', 'track')
+
+
+class UserAlbum(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    album = models.ForeignKey(Album, on_delete=models.CASCADE)
+
+    class Meta:
+        unique_together = ('user', 'album')
+
+
+class ArtistAlbum(models.Model):
+    artist = models.ForeignKey(Artist, on_delete=models.CASCADE)
+    album = models.ForeignKey(Album, on_delete=models.CASCADE)
+
+    class Meta:
+        unique_together = ('artist', 'album')
+
+
+class PlaylistGenre(models.Model):
+    playlist = models.ForeignKey(Playlist, on_delete=models.CASCADE)
+    genre = models.ForeignKey(Genre, on_delete=models.CASCADE)
+
+    class Meta:
+        unique_together = ('playlist', 'genre')
+
+
+class ArtistFollower(models.Model):
+    artist = models.ForeignKey(Artist, on_delete=models.CASCADE)
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+
+    class Meta:
+        unique_together = ('artist', 'user')
